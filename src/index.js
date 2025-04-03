2f014aee8d5349fe2157159bd8a7465fcbdd54bb
Resource-03 <resource3.code4bharat@gmail.com>
2025-04-03 13:53:01 +0530

    completed the datbase conection lec successfully and the error was occuring in env file that was becaose a unnecessary semicolon was given after port number

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path : './.env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT)
+})
+.catch((err) => {
+    console.log(`the database connection failed !! `, err )
+})
 
 
 
@@ -16,7 +23,7 @@ connectDB()
 // (async () => {
 //     try {
 //        await mongoose.connect(`${process.env.MONGO_DB}/${DB_NAME}`)
-//        app.on("errror", (error) => {
+//        app.on("error", (error) => {
 //         console.log("Error", error);
 //         throw error
 //        })
